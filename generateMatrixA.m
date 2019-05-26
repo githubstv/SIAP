6a095f5f22eea7d9ba11a9bfd0090be8f63f1ad4
Tatyana <stv_95@mail.ru>
2019-05-26 22:05:36 +0300

    generate matrix a

--- generateMatrixA.m
+++ generateMatrixA.m
@@ -2,9 +2,9 @@ function A = generateMatrixA(N, levels)
 %GENERATEMATRIXA Summary of this function goes here
 %   Detailed explanation goes here
 
-TEMP = randi(2,N) - ones(N);
 
-for i=1:levels
+for i=1:levels  
+    TEMP = randi(2,N) - ones(N);
     temp= zeros(N) + triu(TEMP, 1) + triu(TEMP,1)';
     A(1:N,((i-1)*N+1):i*N)=temp;
 end
